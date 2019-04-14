1b5ab8623535cbc2c0627885b5e9cb49ebfaa46e
spitchell <mr.spitchell@gmail.com>
2019-04-14 16:06:08 +0100

    gulp tasks getting there

--- gulpfile.js
+++ gulpfile.js
@@ -8,11 +8,35 @@ const autoprefixer = require('autoprefixer');
 const purgecss = require('@fullhuman/postcss-purgecss');
 const htmlmin = require('gulp-htmlmin');
 
+// +++++++++++++++//
+// BUILD FOR DEV //
+// +++++++++++++++//
+
+// No real reason to minify into the dev build but it gets the files there without adding gulp-copy as another dep
+
 function minifyHtml() {
   return gulp.src('src/html/*.html')
     .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest('dist/'));
+    .pipe(gulp.dest('dist/'))
+    .pipe(gulp.dest('build/'));
 }
+
+// This unpacks the styles.css file into a master CSS containing all the tailwind classes and any custom CSS added to the sheet
+
+function css() {
+  const plugins = [
+    tailwindcss('./tailwind.js'),
+    autoprefixer({browsers: ['last 1 version']})
+  ];
+  return gulp.src('src/css/styles.css')
+    .pipe(postcss(plugins))
+    .pipe(gulp.dest('dist/'));
+};
+
+// +++++++++++++++//
+// BUILD FOR PROD //
+// +++++++++++++++//
+
 
 function minifyCss() {
   const plugins = [
@@ -25,28 +49,16 @@ function minifyCss() {
     .pipe(gulp.dest('build/'));
 }
 
-function css() {
-  const plugins = [
-    tailwindcss('./tailwind.js'),
-    autoprefixer({browsers: ['last 1 version']})
-  ];
-  return gulp.src('src/css/styles.css')
-    .pipe(postcss(plugins))
-    .pipe(gulp.dest('dist/'));
-};
-
 function dev() {
   gulp.watch(['src/css/styles.css','src/html/**/*.html']);
 };
 
-
-// function watchFiles() {
-//   gulp.watch('src/css/styles.css', css);
-//   gulp.watch('src/html/*.html', minify);
-// }
+gulp.task('html', minifyHtml);
+gulp.task('css', minifyCss);
 
 gulp.task('refresh', gulp.parallel(minifyHtml,css));
 
 gulp.task(dev, 'refresh');
+
 
 // gulp.task('build', series(minifyHtml,minifyCss))
